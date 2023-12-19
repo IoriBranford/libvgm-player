94da79a7f5b45f7928e268deb2c5dd2b3985cb3b
IoriBranford <ioribranford@gmail.com>
2023-12-19 00:12:30 -0800

    Add dllexport to PlayerC functions.

--- player/playerc.h
+++ player/playerc.h
@@ -4,6 +4,12 @@
 #include "../stdtype.h"
 #include "utils/DataLoader.h"
 
+#ifdef _MSC_VER
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -15,19 +21,19 @@ struct PlayerC {
     DATA_LOADER *loader;
 };
 
-PlayerC* PlayerC_NewVGM();
-UINT8 PlayerC_SetOutputSettings(PlayerC *p, UINT32 smplRate, UINT8 channels, UINT8 smplBits, UINT32 smplBufferLen);
-void PlayerC_SetLoopCount(PlayerC *p, UINT32 loopCount);
+DLLEXPORT PlayerC* PlayerC_NewVGM();
+DLLEXPORT UINT8 PlayerC_SetOutputSettings(PlayerC *p, UINT32 smplRate, UINT8 channels, UINT8 smplBits, UINT32 smplBufferLen);
+DLLEXPORT void PlayerC_SetLoopCount(PlayerC *p, UINT32 loopCount);
 
-UINT8 PlayerC_LoadFile(PlayerC *p, const char *filename);
-UINT8 PlayerC_LoadMemory(PlayerC *p, const UINT8 *data, UINT32 length);
+DLLEXPORT UINT8 PlayerC_LoadFile(PlayerC *p, const char *filename);
+DLLEXPORT UINT8 PlayerC_LoadMemory(PlayerC *p, const UINT8 *data, UINT32 length);
 
-UINT8 PlayerC_Start(PlayerC *p);
-UINT32 PlayerC_Render(PlayerC *p, UINT32 bufSize, void *data);
-UINT8 PlayerC_FadeOut(PlayerC *p);
-UINT8 PlayerC_Stop(PlayerC *p);
+DLLEXPORT UINT8 PlayerC_Start(PlayerC *p);
+DLLEXPORT UINT32 PlayerC_Render(PlayerC *p, UINT32 bufSize, void *data);
+DLLEXPORT UINT8 PlayerC_FadeOut(PlayerC *p);
+DLLEXPORT UINT8 PlayerC_Stop(PlayerC *p);
 
-void PlayerC_delete(PlayerC *p);
+DLLEXPORT void PlayerC_delete(PlayerC *p);
 
 #ifdef __cplusplus
 } // extern "C"

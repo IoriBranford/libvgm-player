5dec86b21ed10cd02f89c7ff176f8393a7b6cab4
IoriBranford <ioribranford@gmail.com>
2023-12-18 18:02:41 -0800

    Start C interface.

---
+++ player/playerc.cpp
@@ -0,0 +1,79 @@
+#include "playerc.h"
+#include "playera.hpp"
+#include "vgmplayer.hpp"
+#include "utils/FileLoader.h"
+#include "utils/MemoryLoader.h"
+
+PlayerC* PlayerC_NewVGM() {
+    PlayerC *p = new PlayerC;
+    p->player = new PlayerA;
+    p->player->RegisterPlayerEngine(new VGMPlayer);
+    p->loader = nullptr;
+    return p;
+}
+
+UINT8 PlayerC_SetOutputSettings(PlayerC *p, UINT32 smplRate, UINT8 channels, UINT8 smplBits, UINT32 smplBufferLen) {
+    return p->player->SetOutputSettings(smplRate, channels, smplBits, smplBufferLen);
+}
+
+void PlayerC_SetLoopCount(PlayerC *p, UINT32 loops) {
+    p->player->SetLoopCount(loops);
+}
+
+UINT8 PlayerC_LoadData(PlayerC *p, DATA_LOADER *loader) {
+    if (!loader)
+        return ENOMEM;
+
+    UINT8 error = DataLoader_Load(loader);
+    if (!error) {
+        error = p->player->LoadFile(loader);
+    }
+    if (!error) {
+        p->loader = loader;
+    }
+
+    if (error)
+        DataLoader_Deinit(loader);
+    return error;
+}
+
+UINT8 PlayerC_LoadFile(PlayerC *p, const char *filename) {
+    return PlayerC_LoadData(p, FileLoader_Init(filename));
+}
+
+UINT8 PlayerC_LoadMemory(PlayerC *p, const UINT8 *data, UINT32 length) {
+    DATA_LOADER *loader = MemoryLoader_Init(data, length);
+    if (!loader)
+        return ENOMEM;
+
+    UINT8 error = PlayerC_LoadData(p, loader);
+    DataLoader_Deinit(loader);
+    return error;
+}
+
+UINT8 PlayerC_Start(PlayerC *p) {
+    return p->player->Start();
+}
+
+UINT32 PlayerC_Render(PlayerC *p, UINT32 bufSize, void *data) {
+    return p->player->Render(bufSize, data);
+}
+
+UINT8 PlayerC_Stop(PlayerC *p) {
+    return p->player->Stop();
+}
+
+void PlayerC_delete(PlayerC *p) {
+    if (p != nullptr) {
+        if (p->player != nullptr) {
+            p->player->Stop();
+            p->player->UnloadFile();
+            p->player->UnregisterAllPlayers();
+            delete p->player;
+        }
+        if (p->loader != nullptr) {
+            DataLoader_Deinit(p->loader);
+        }
+        delete p;
+    }
+}
---
+++ player/playerc.h
@@ -0,0 +1,35 @@
+#ifndef PlayerC_C_H
+#define PlayerC_C_H
+
+#include "../stdtype.h"
+#include "utils/DataLoader.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct PlayerA;
+
+struct PlayerC {
+    PlayerA *player;
+    DATA_LOADER *loader;
+};
+
+PlayerC* PlayerC_NewVGM();
+UINT8 PlayerC_SetOutputSettings(PlayerC *p, UINT32 smplRate, UINT8 channels, UINT8 smplBits, UINT32 smplBufferLen);
+void PlayerC_SetLoopCount(PlayerC *p, UINT32 loopCount);
+
+UINT8 PlayerC_LoadFile(PlayerC *p, const char *filename);
+UINT8 PlayerC_LoadMemory(PlayerC *p, const UINT8 *data, UINT32 length);
+
+UINT8 PlayerC_Start(PlayerC *p);
+UINT32 PlayerC_Render(PlayerC *p, UINT32 bufSize, void *data);
+UINT8 PlayerC_Stop(PlayerC *p);
+
+void PlayerC_delete(PlayerC *p);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif

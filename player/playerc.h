b3016d20ecfb29ccf6b5134a39028f8c58f2e7d9
IoriBranford <ioribranford@gmail.com>
2023-12-19 17:25:03 -0800

    Replace dllexports with WINDOWS_EXPORT_ALL_SYMBOLS property.

--- player/playerc.h
+++ player/playerc.h
@@ -3,12 +3,6 @@
 
 #include "utils/DataLoader.h"
 
-#ifdef _MSC_VER
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -20,20 +14,20 @@ typedef struct PlayerC {
     DATA_LOADER *loader;
 } PlayerC;
 
-DLLEXPORT PlayerC* PlayerC_NewVGM();
-DLLEXPORT uint8_t PlayerC_SetOutputSettings(PlayerC *p, uint32_t smplRate, uint8_t channels, uint8_t smplBits, uint32_t smplBufferLen);
-DLLEXPORT void PlayerC_SetLoopCount(PlayerC *p, uint32_t loopCount);
-DLLEXPORT void PlayerC_SetFadeSeconds(PlayerC *p, uint32_t seconds);
+PlayerC* PlayerC_NewVGM();
+uint8_t PlayerC_SetOutputSettings(PlayerC *p, uint32_t smplRate, uint8_t channels, uint8_t smplBits, uint32_t smplBufferLen);
+void PlayerC_SetLoopCount(PlayerC *p, uint32_t loopCount);
+void PlayerC_SetFadeSeconds(PlayerC *p, uint32_t seconds);
 
-DLLEXPORT uint8_t PlayerC_LoadFile(PlayerC *p, const char *filename);
-DLLEXPORT uint8_t PlayerC_LoadMemory(PlayerC *p, const uint8_t *data, uint32_t length);
+uint8_t PlayerC_LoadFile(PlayerC *p, const char *filename);
+uint8_t PlayerC_LoadMemory(PlayerC *p, const uint8_t *data, uint32_t length);
 
-DLLEXPORT uint8_t PlayerC_Start(PlayerC *p);
-DLLEXPORT uint32_t PlayerC_Render(PlayerC *p, uint32_t bufSize, void *data);
-DLLEXPORT uint8_t PlayerC_FadeOut(PlayerC *p);
-DLLEXPORT uint8_t PlayerC_Stop(PlayerC *p);
+uint8_t PlayerC_Start(PlayerC *p);
+uint32_t PlayerC_Render(PlayerC *p, uint32_t bufSize, void *data);
+uint8_t PlayerC_FadeOut(PlayerC *p);
+uint8_t PlayerC_Stop(PlayerC *p);
 
-DLLEXPORT void PlayerC_delete(PlayerC *p);
+void PlayerC_delete(PlayerC *p);
 
 #ifdef __cplusplus
 } // extern "C"
